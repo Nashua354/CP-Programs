7b024a8addea5f11c211b83432d6ddad21e8f5c9
Nitish Sahani <nitishssahani@gmail.com>
2018-06-13 21:21:29 +0530

    Hulk Day one.

---
+++ Hulk.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,i=0;
+    cin>>n;
+    while(i!=n){
+        if(i==n-1){
+            if(i%2==0)
+        cout<<"I hate it";
+    else
+        cout<<"I love it";
+        }
+        else{
+        if(i%2==0)
+        cout<<"I hate that ";
+    else
+        cout<<"I love that ";
+        }
+
+    i++;
+    }
+return 0;
+}
